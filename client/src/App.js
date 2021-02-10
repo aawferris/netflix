aed43eccae4fad7e9ce2552ec715b35cf3cddaed
Andy Ferris <aawferris@aawferris.home>
2021-02-10 17:04:46 -0500

    add rendering for rows by category, map over JSON to return image and title, add styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
-import react from "react";
+import React from "react";
+import requests from "./requests";
+import Row from "./Row";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>We're doing it!</h1>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
